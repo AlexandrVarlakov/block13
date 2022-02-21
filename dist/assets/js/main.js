91da60e716e1767d1882a18d15ecd068bc4000d7
AlexandrVarlakov <66496459+AlexandrVarlakov@users.noreply.github.com>
2022-02-22 02:39:46 +0300

    create all section

---
+++ dist/assets/js/main.js
@@ -0,0 +1,50 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // конечная дата
+    const deadline = new Date(new Date().getFullYear(), new Date().getMonth() + 1, 34, 12);
+    // id таймера
+    let timerId = null;
+    // склонение числительных
+    function declensionNum(num, words) {
+      return words[(num % 100 > 4 && num % 100 < 20) ? 2 : [2, 0, 1, 1, 1, 2][(num % 10 < 5) ? num % 10 : 5]];
+    }
+    // вычисляем разницу дат и устанавливаем оставшееся времени в качестве содержимого элементов
+    function countdownTimer() {
+      const diff = deadline - new Date();
+      if (diff <= 0) {
+        clearInterval(timerId);
+      }
+      const days = diff > 0 ? Math.floor(diff / 1000 / 60 / 60 / 24) : 0;
+      const hours = diff > 0 ? Math.floor(diff / 1000 / 60 / 60) % 24 : 0;
+      const minutes = diff > 0 ? Math.floor(diff / 1000 / 60) % 60 : 0;
+      const seconds = diff > 0 ? Math.floor(diff / 1000) % 60 : 0;
+      $days.textContent = days < 10 ? '0' + days : days;
+      $hours.textContent = hours < 10 ? '0' + hours : hours;
+      $minutes.textContent = minutes < 10 ? '0' + minutes : minutes;
+      $seconds.textContent = seconds < 10 ? '0' + seconds : seconds;
+
+
+      let daysLabel = document.querySelector('.timer__label--days');
+      let hoursLabel = document.querySelector('.timer__label--hours');
+      let minutesLabel = document.querySelector('.timer__label--minutes');
+      let secondsLabel = document.querySelector('.timer__label--seconds');
+
+      daysLabel.innerHTML = declensionNum(days, ['день', 'дня', 'дней']);
+      hoursLabel.innerHTML = declensionNum(hours, ['час', 'часа', 'часов']);
+      minutesLabel.innerHTML = declensionNum(minutes, ['минута', 'минуты', 'минут']);
+      secondsLabel.innerHTML = declensionNum(seconds, ['секунда', 'секунды', 'секунд']);
+
+      $days.dataset.title = declensionNum(days, ['день', 'дня', 'дней']);
+      $hours.dataset.title = declensionNum(hours, ['час', 'часа', 'часов']);
+      $minutes.dataset.title = declensionNum(minutes, ['минута', 'минуты', 'минут']);
+      $seconds.dataset.title = declensionNum(seconds, ['секунда', 'секунды', 'секунд']);
+    }
+    // получаем элементы, содержащие компоненты даты
+    const $days = document.querySelector('.timer__day-count');
+    const $hours = document.querySelector('.timer__hours-count');
+    const $minutes = document.querySelector('.timer__minutes-count');
+    const $seconds = document.querySelector('.timer__seconds-count');
+    // вызываем функцию countdownTimer
+    countdownTimer();
+    // вызываем функцию countdownTimer каждую секунду
+    timerId = setInterval(countdownTimer, 1000);
+  });
