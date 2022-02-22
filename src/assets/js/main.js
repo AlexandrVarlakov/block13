1988f1d27a293756ec0d3b7ded35a152d24d07d6
AlexandrVarlakov <66496459+AlexandrVarlakov@users.noreply.github.com>
2022-02-22 20:46:07 +0300

    create validation for form + sript for include video file

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -48,3 +48,155 @@ document.addEventListener('DOMContentLoaded', function () {
     // вызываем функцию countdownTimer каждую секунду
     timerId = setInterval(countdownTimer, 1000);
   });
+
+
+  let inputsPhone = document.querySelectorAll('input[name = "phone"]');
+
+  if (inputsPhone.length > 0) {
+      inputsPhone.forEach( (input) => {
+          IMask(input, {mask: '+{7}( 000 ) 000 - 00 - 00'});
+      })
+  }
+let fInputs = document.querySelectorAll('.form__tinput');
+
+fInputs.forEach( (input) => {
+  input.oninput = function(){
+    if ( this.value.length > 0 ){
+      this.classList.add('inputed');
+    } else {
+      this.classList.remove('inputed');
+    }
+  }
+} )
+
+
+document.addEventListener("DOMContentLoaded", function(){
+
+  let video = document.querySelector('video');
+
+  if ( document.body.scrollWidth  > 1024 ){
+    video.src = "assets/img/preview-a.mp4";
+  } else if( document.body.scrollWidth  < 1024  && document.body.scrollWidth  > 580  ){
+    video.src = "assets/img/preview-a-md.mp4";
+  } else {
+    video.src = "assets/img/preview-a-sm.mp4";
+  }
+  
+});
+
+
+let errMsg = document.querySelectorAll('.err-msg');
+
+
+errMsg.forEach( (msg) => {
+  msg.onclick = function(){
+    let wrap = this.closest('.form__iwrap');
+
+    wrap.classList.remove('err-wrap');
+
+    wrap.querySelector('input').focus();
+
+  }
+});
+
+
+let form = document.querySelector('.form');
+
+
+form.onsubmit = function(event){
+  event.preventDefault();
+  
+  let err = 0;
+
+
+  let nameField = this.querySelector('input[name = "name"]');
+  let nameValue = nameField.value;
+
+  if (nameValue.length < 1){
+    let wrap = nameField.closest('.form__iwrap');
+    wrap.querySelector('.err-text').innerHTML = "Поле не может быть пустым";
+    wrap.classList.add('err-wrap');
+    err++;
+  }
+
+  let phoneField = this.querySelector('input[name = "phone"]');
+  let phoneValue = phoneField.value;
+
+  if (phoneValue.length < 1){
+    let wrap = phoneField.closest('.form__iwrap');
+    wrap.querySelector('.err-text').innerHTML = "Поле не может быть пустым";
+    wrap.classList.add('err-wrap');
+    err++;
+  }
+
+
+  if (phoneValue.length < 23 && phoneValue.length > 0){
+    let wrap = phoneField.closest('.form__iwrap');
+    wrap.querySelector('.err-text').innerHTML = "Не корректное значение";
+    wrap.classList.add('err-wrap');
+    err++;
+  }
+
+
+
+  let emailField = this.querySelector('input[name = "email"]');
+  let emailValue = emailField.value;
+  if ((emailValue.includes('.') === false  || emailValue.includes('@') === false || emailValue.length < 6)){
+    let wrap = emailField.closest('.form__iwrap');
+    wrap.querySelector('.err-text').innerHTML = "Не корректное значение";
+    wrap.classList.add('err-wrap');
+    err++;
+  }
+  if (emailValue.length < 1){
+    let wrap = emailField.closest('.form__iwrap');
+    wrap.querySelector('.err-text').innerHTML = "Поле не может быть пустым";
+    wrap.classList.add('err-wrap');
+    err++;
+  }
+
+  if (err === 0){
+    fetch(phpScript, { 
+      method: "POST",
+      body: data_body,   
+      headers:{"content-type": "application/x-www-form-urlencoded"} 
+      })
+      
+    .then( (response) => {
+            if (response.status !== 200) {           
+                return Promise.reject();
+                
+            }   
+        
+
+
+    console.log("Почта отправлена");
+    inputs.forEach ( (input) => {
+        input.value = '';
+    });
+    return response.text()
+    })
+    .then(i => console.log(i))
+    .catch(() => console.log('ошибка')); 
+    }
+  
+  
+}
+
+let soundBtn = document.querySelector('.sound-btn');
+
+soundBtn.onclick = function(){
+  let video = document.querySelector('video');
+  if (this.getAttribute('data-value') === "1" ){
+
+    this.setAttribute('data-value', '0');
+    video.muted = true;
+
+    
+  } else{
+    this.setAttribute('data-value', '1');
+    video.muted = false;
+  }
+
+
+  
+}
